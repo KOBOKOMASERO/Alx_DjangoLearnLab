501159cdd33bd3b5c9a6d75c66113b4685da7efb
KOBOKOMASERO <kobokophilip@gmail.com>
2025-11-08 17:19:01 +0300

    user_authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from . import views
-from .views import LibraryDetailView
-from .views import list_books
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register_view,
+    login_view,
+    logout_view
+)
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
