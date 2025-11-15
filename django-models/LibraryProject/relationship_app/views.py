cab5a75ac588ce8e679c4330fa7f583718b94237
KOBOKOMASERO <kobokophilip@gmail.com>
2025-11-16 02:08:48 +0300

    updates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,9 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test, permission_required
-from .models import Book, Library, Author
+from .models import Book
+from .models import Library
+from .models import Author
 
 # -----------------------
 # Book and Library Views
