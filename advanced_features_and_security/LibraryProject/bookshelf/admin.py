9f466fc0cd7d230fefd5dec8affcff429ec2f173
KOBOKOMASERO <kobokophilip@gmail.com>
2025-11-16 18:30:12 +0300

    security_features

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,38 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser, UserProfile
 
+# -----------------------
+# Custom User Admin
+# -----------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Include extra fields in the admin form
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Register CustomUser with the custom UserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# -----------------------
+# UserProfile Admin
+# -----------------------
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    search_fields = ('user__username', 'role')
+
+# -----------------------
+# Book Admin
+# -----------------------
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  
-    search_fields = ('title', 'author')                     
-    list_filter = ('publication_year',)                     
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
 
-# Register with custom admin
+# Register Book
 admin.site.register(Book, BookAdmin)
-
-
-# Register your models here.
