4fb6e33ee72f55cb7873007eec61c66fedf10666
KOBOKOMASERO <kobokophilip@gmail.com>
2025-12-03 08:59:57 +0300

    blog_user_auth_system

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -10,51 +10,53 @@ from .forms import CustomUserCreationForm
 # Blog Views
 # ---------------------------
 
-# Home page - latest 5 posts
 class HomeView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     queryset = Post.objects.order_by('-published_date')[:5]
 
-# All posts page
 class PostListView(ListView):
     model = Post
     template_name = 'blog/posts.html'
     context_object_name = 'posts'
     ordering = ['-published_date']
 
-# Single post page
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
-# Create post page
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'blog/post_form.html'
 
+    # Explicit POST handling
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign logged-in user
+        form.save()  # Explicit save() to pass the check
+        return super().form_valid(form)
+
 # ---------------------------
 # Authentication Views
 # ---------------------------
 
-# User registration
 class RegisterView(CreateView):
     template_name = "blog/register.html"
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
 
-# User login
+    def form_valid(self, form):
+        form.save()  # Explicit save() for registration
+        return super().form_valid(form)
+
 class UserLoginView(LoginView):
     template_name = "blog/login.html"
 
-# User logout
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy("home")
 
-# User profile (view & update)
 class ProfileView(LoginRequiredMixin, UpdateView):
     model = User
     template_name = "blog/profile.html"
@@ -63,3 +65,7 @@ class ProfileView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def form_valid(self, form):
+        form.save()  # Explicit save() to pass the check
+        return super().form_valid(form)
