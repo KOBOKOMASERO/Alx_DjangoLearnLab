df1b9e5e6922a021dbc491473606149e053845eb
KOBOKOMASERO <kobokophilip@gmail.com>
2025-12-05 17:54:11 +0300

    blog/models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -20,6 +20,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     class Meta:
         ordering = ['-created_at']  # newest comments first
 
