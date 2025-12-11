937431dc04c37da3aedb1de1f7f9aeb88f3ed5cf
KOBOKOMASERO <kobokophilip@gmail.com>
2025-12-11 13:19:10 +0300

    accounts/serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,23 @@
-from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture', 'followers']
+        read_only_fields = ['id', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'email', 'password', 'first_name', 'last_name', 'bio']
 
     def create(self, validated_data):
-        User = get_user_model()
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
@@ -18,6 +26,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             last_name=validated_data.get('last_name', ''),
             bio=validated_data.get('bio', '')
         )
-        # create token
+        # create token for the user
         Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.ValidationError('Invalid credentials')
+        attrs['user'] = user
+        return attrs
